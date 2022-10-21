c1591fb90ce3701e69c975171954ae4948a27e7c
KlintonICT <chhunklinton91@gmail.com>
2022-10-21 17:28:24 +0700

    add arrays

--- exercise/arrays/arrays.go
+++ exercise/arrays/arrays.go
@@ -4,9 +4,9 @@
 //
 //--Requirements:
 //* Using an array, create a shopping list with enough room
-//  for 4 products
-//  - Products must include the price and the name
-//* Insert 3 products into the array
+//  for 4 Product
+//  - Product must include the price and the name
+//* Insert 3 Product into the array
 //* Print to the terminal:
 //  - The last item on the list
 //  - The total number of items
@@ -18,6 +18,38 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	price int
+	name  string
+}
 
+func printStats(list [4]Product) {
+	var cost, totalItems int
+
+	for i := 0; i < len(list); i++ {
+		item := list[i]
+		cost += item.price
+
+		if item.name != "" {
+			totalItems += 1
+		}
+	}
+
+	fmt.Println("Last item on the list:", list[totalItems-1])
+	fmt.Println("Total number of items:", totalItems)
+	fmt.Println("Total cost", cost)
+}
+
+func main() {
+	shoppingList := [4]Product{
+		{10, "Milk"},
+		{20, "Bread"},
+		{30, "Eggs"},
+	}
+
+	printStats(shoppingList)
+
+	shoppingList[3] = Product{40, "Cheese"}
+
+	printStats(shoppingList)
 }
